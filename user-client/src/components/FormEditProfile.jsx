72ca534b0f1ab0fb71d3dd006558cba8cb856e6a
Cakrabsva <cakrabilisairo.va@gmail.com>
2025-07-27 16:42:28 +0700

    seperate edit form and fixing bug

---
+++ user-client/src/components/FormEditProfile.jsx
@@ -0,0 +1,97 @@
+import "../index.css"
+import { useState } from "react";
+
+export default function FormEditProfile (props) {
+
+    const {first_name, last_name, born_date, gender, bio} = props.userData.Profile
+    const [form, setForm] = useState({first_name, last_name, born_date, gender, bio});
+
+    const handleChangeForm = (e) => {
+        const {name, value} = e.target
+        setForm((prev)=>({...prev, [name]: value}))
+    }
+
+    const handleOnSubmit = (e) => {
+        e.preventDefault()
+        console.log(form)
+    }
+
+    return (
+        <div className="justify-items-center">
+            <form 
+            action=""
+            onSubmit={handleOnSubmit}>
+                <div className="space-y-6">
+                    <div className="flex space-x-2">
+                        <div className="">
+                            <label htmlFor="first_name" className="font-semibold ml-1">First Name</label> <br />
+                            <input 
+                                type="text" 
+                                name="first_name" 
+                                id="first_name"
+                                value={form.first_name || ''}
+                                onChange={handleChangeForm}
+                                onKeyDown={(e) => {
+                                    if (e.key === ' ') e.preventDefault();
+                                }}
+                                className="border border-gray-300 rounded-lg w-46 h-10 px-3"/>
+                        </div>
+                        <div>
+                            <label htmlFor="last_name" className="font-semibold ml-1">Last Name</label> <br />
+                            <input 
+                                type="text" 
+                                name="last_name" 
+                                id="last_name"
+                                value={form.last_name || ''}
+                                onChange={handleChangeForm}
+                                onKeyDown={(e) => {
+                                    if (e.key === ' ') e.preventDefault();
+                                }}
+                                className="border border-gray-300 rounded-lg w-46 h-10 px-3"/>
+                        </div>
+                    
+                    </div>
+                    <div className="flex space-x-2">
+                        <div>
+                            <label htmlFor="born_date" className="font-semibold ml-1">Born Date</label> <br />
+                            <input 
+                                type="date" 
+                                name="born_date" 
+                                id="born_date"
+                                value={form.born_date || ''}
+                                onChange={handleChangeForm}
+                                className="border border-gray-300 rounded-lg w-46 h-10 px-3"/>
+                        </div>
+                        <div>
+                            <label htmlFor="gender" className="font-semibold ml-1">Gender</label> <br />
+                            <select 
+                                name="gender" 
+                                id="gender"
+                                value={form.gender || ''}
+                                onChange={handleChangeForm}
+                                className="border border-gray-300 rounded-lg w-46 h-10 px-3">
+                                    <option value="">Select</option>
+                                    <option value="male">Male</option>
+                                    <option value="female">Female</option>
+                                </select>
+                        </div>
+                    </div>
+                
+                        <div>
+                            <label htmlFor="bio" className="font-semibold ml-1">Bio</label> <br />
+                            <textarea 
+                                name="bio" 
+                                id="bio"
+                                wrap="soft"
+                                value={form.bio || ''}
+                                onChange={handleChangeForm}
+                                className="border border-gray-300 rounded-lg w-full h-32 px-3 py-3 text-wrap"></textarea>
+                        </div>
+                    <div className="flex justify-end">
+                        <button type="submit" className="rounded-full bg-yellow-300 hover:bg-yellow-400 cursor-pointer font-semibold py-3 px-8">Save</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
--- user-client/src/pages/EditProfile.jsx
+++ user-client/src/pages/EditProfile.jsx
@@ -3,21 +3,20 @@ import { ChevronLeft } from "lucide-react"
 import { Link } from 'react-router-dom';
 import avatarReplacer from "../assets/profile_picture_replacer.jpg"
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
+import { useEffect} from "react";
 import { fetchGlobalData } from "../features/global-data/globalData";
 import Loading from "../components/Loading";
+import FormEditProfile from "../components/FormEditProfile";
 
 export default function EditProfile () {
 
-     let {user, isLoading} = useSelector(state => state.globalData)
-
-
+    const {user, isLoading} = useSelector(state => state.globalData)
     const dispatch = useDispatch()
-
+    
     useEffect(() => {
         dispatch(fetchGlobalData());
     }, [dispatch]);
-
+    
     if(isLoading) {
         return (
             <Loading />
@@ -43,74 +42,7 @@ export default function EditProfile () {
                     <a>Change profile avatar</a>
                 </div>
             </div>
-            
-            <div className="justify-items-center">
-                <form 
-                action="">
-                    <div className="space-y-6">
-                        <div className="flex space-x-2">
-                            <div className="">
-                                <label htmlFor="first_name" className="font-semibold ml-1">First Name</label> <br />
-                                <input 
-                                    type="text" 
-                                    name="first_name" 
-                                    id="first_name"
-                                    value={user.Profile.first_name || ''}
-                                    className="border border-gray-300 rounded-lg w-46 h-10 px-3"/>
-                            </div>
-                            <div>
-                                <label htmlFor="last_name" className="font-semibold ml-1">Last Name</label> <br />
-                                <input 
-                                    type="text" 
-                                    name="last_name" 
-                                    id="last_name"
-                                    value={user.Profile.last_name || ''}
-                                    className="border border-gray-300 rounded-lg w-46 h-10 px-3"/>
-                            </div>
-                        
-                        </div>
-                        <div className="flex space-x-2">
-                            <div>
-                                <label htmlFor="born_date" className="font-semibold ml-1">Born Date</label> <br />
-                                <input 
-                                    type="date" 
-                                    name="born_date" 
-                                    id="born_date"
-                                    value={user.Profile.born_date || ''}
-                                    className="border border-gray-300 rounded-lg w-46 h-10 px-3"/>
-                            </div>
-                            <div>
-                                <label htmlFor="gender" className="font-semibold ml-1">Gender</label> <br />
-                                <select 
-                                    name="gender" 
-                                    id="gender"
-                                    value={user.Profile.gender || ''}
-                                    className="border border-gray-300 rounded-lg w-46 h-10 px-3">
-                                        <option value="">Select</option>
-                                        <option value="male">Male</option>
-                                        <option value="female">Female</option>
-                                    </select>
-                            </div>
-                        </div>
-                    
-                            <div>
-                                <label htmlFor="bio" className="font-semibold ml-1">Bio</label> <br />
-                                <textarea 
-                                    name="bio" 
-                                    id="bio"
-                                    wrap="soft"
-                                    value={user.Profile.bio || ''}
-                                    className="border border-gray-300 rounded-lg w-full h-32 px-3 py-3 text-wrap"></textarea>
-                            </div>
-                        <div className="flex justify-end">
-                            <button type="submit" className="rounded-full bg-yellow-300 hover:bg-yellow-400 cursor-pointer font-semibold py-3 px-8">Save</button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-            
-
-
+        <FormEditProfile userData={user}/>
         </div>
     )
 }
